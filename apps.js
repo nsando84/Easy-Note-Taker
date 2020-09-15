8be332463736e1561f5c6cd3448006f45ffe526e
nicksandoval <sand8val@gmail.com>
2020-09-15 16:46:32 -0700

    formatting okay

--- apps.js
+++ apps.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const PORT = process.env.PORT 
+const PORT = process.env.PORT || 3050
 
 
 // const logger = (req, res, next) => {
