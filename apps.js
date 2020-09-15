68560b6a01673b5946c86429af75cca36ee59acb
nicksandoval <sand8val@gmail.com>
2020-09-15 15:14:02 -0700

    trying

--- apps.js
+++ apps.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const PORT = process.env.PORT || 3050;
+const PORT = process.env.PORT 
 
 
 // const logger = (req, res, next) => {
